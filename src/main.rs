515cbcea6e74f4f86a11a451ff834357779a2094
hoggetaylor <hoggetaylor@gmail.com>
2017-08-15 23:04:16 -0600

    generate stego codes every second

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,27 @@
-#[macro_use]
 extern crate stdweb;
+extern crate rand;
 extern crate matrix_code;
 
+use stdweb::web::{document, INode};
+use std::time::Duration;
+use matrix_code::Stego;
+use matrix_code::render::RenderOptions;
+
 fn main() {
-    println!("Hello, world!");
+    stdweb::initialize();
+
+    let div = document().query_selector("stego-container").unwrap();
+    loop {
+        div.set_text_content(&random_code());
+        std::thread::sleep(Duration::from_secs(1));
+    }
+}
+
+fn random_code() -> String {
+    let id = ::rand::random();
+    let stego = Stego::new();
+    let bytes = stego.render(id, &RenderOptions::default());
+    unsafe {
+        String::from_utf8_unchecked(bytes)
+    }
 }
